d4c50cd9f169a8ad3c5f98de32e2af4782c50a31
Gonzalo Robinson <gonchyrobinson@gmail.com>
2023-05-18 19:03:11 -0300

    Ejercicio2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,27 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:" +a);
-Console.WriteLine("valor de b:"+b);
-*///EJercicio 1
-float a;
-Console.WriteLine("Ingrese el numero que desea invertir:  ");
-string? b= Console.ReadLine();
-bool anda=float.TryParse(b,out a);
-if (anda)
+const float INDEFINIDO = 9999;
+int opcion;
+Console.WriteLine("\n=====================MENU===================\n1-Sumar\n2-Resta\n3-Multiplicar\n4-Dividir\n\nOpcion Elegida:   ");
+string? opcionString=Console.ReadLine();
+Console.WriteLine("\nOPERADORES\nOperador 1:  ");
+string? operador1string=Console.ReadLine();
+Console.WriteLine("Operador 2: ");
+string? operador2string=Console.ReadLine();
+bool andaOperacion=int.TryParse(opcionString, out opcion);
+float operador1;
+float operador2;
+bool andaOp1=float.TryParse(operador1string,out operador1);
+bool andaOp2=float.TryParse(operador2string, out operador2);
+float resultado;
+if (andaOperacion && andaOp1 && andaOp2)
 {
-    if (a>0)
-    {    
-        float invertido= 1/a;
-        Console.WriteLine("El numero invertido es: "+invertido);
-    }else
+    switch (opcion)
     {
-        Console.WriteLine("El numero ingresado no es mayor a 0");
+        case 1:
+            resultado=operador1+operador2;
+        break;
+        case 2:
+            resultado=operador1-operador2;
+        break;
+        case 3: 
+            resultado=operador1*operador2;
+        break;
+        case 4:
+            if (operador2!=0)
+            {
+                resultado=operador1/operador2;
+            }else
+            {
+                resultado=INDEFINIDO;
+            }
+        break;
+        default:
+            resultado=INDEFINIDO;
+        break;
     }
+    Console.WriteLine("Resultado= "+resultado);
 }else
 {
-    Console.WriteLine("El numero " +b+ " no es valido");
+    Console.WriteLine("Hay un dato erroneo");
 }
