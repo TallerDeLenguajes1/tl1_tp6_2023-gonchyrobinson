3eaccf7a84c1119a32d9e7545f573e02d97dd2ab
Gonzalo Robinson <gonchyrobinson@gmail.com>
2023-05-18 18:12:03 -0300

    Ejercicio1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,8 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Console.WriteLine("Hello, World!");
 int a;
 int b;
 a=10;
 b=a;
 Console.WriteLine("valor de a:" +a);
 Console.WriteLine("valor de b:"+b);
+*///EJercicio 1
+float a;
+Console.WriteLine("Ingrese el numero que desea invertir:  ");
+string? b= Console.ReadLine();
+bool anda=float.TryParse(b,out a);
+if (anda)
+{
+    if (a>0)
+    {    
+        float invertido= 1/a;
+        Console.WriteLine("El numero invertido es: "+invertido);
+    }else
+    {
+        Console.WriteLine("El numero ingresado no es mayor a 0");
+    }
+}else
+{
+    Console.WriteLine("El numero " +b+ " no es valido");
+}
